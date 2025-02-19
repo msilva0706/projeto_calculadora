382dcb7c1a8a045c301e192f4a7586de616e5ecb
msilva0706 <marcio070690@gmail.com>
2025-02-18 22:22:25 -0300

    Update todos_comandos.sh

--- todos_comandos.sh
+++ todos_comandos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+no terminal use o comando echo eo nome do arquivo para executar 
+
 echo "todo os comandos usados ate aqui segue abaixo a lista"
 
 echo "mkdir / cd / cd.. / ls / ll / chmod com letras e numeros / cat"
