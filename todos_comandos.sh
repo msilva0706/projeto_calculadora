14f22caa36013337cd38fbd9fd82572cda4e4db7
msilva0706 <marcio070690@gmail.com>
2025-02-18 22:14:23 -0300

    Add files via upload

---
+++ todos_comandos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "todo os comandos usados ate aqui segue abaixo a lista"
+
+echo "mkdir / cd / cd.. / ls / ll / chmod com letras e numeros / cat"
+
+echo "saindo da lista de comandos"
